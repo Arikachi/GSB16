4a2c0535eb10bb248da682defec905174144c44f
Minh Trí <88954074+Arikachi@users.noreply.github.com>
2022-07-29 20:43:53 +0700

    upload file 29/7/2022

---
+++ b1_print.py
@@ -0,0 +1,24 @@
+# print('arika')
+# print('''sdfasd
+# asfasd
+# fsd'''
+# )
+# print('dòng 1 \n dòng 2')
+# print('3', end=',')
+# print('4', end=',')
+# print('5')
+
+# print('5', sep='000')
+
+# userName = "Tri"
+# age = 17
+# adress = 'Ha Noi, Viet Nam'
+# print(userName)
+# print(age)
+# print(adress)
+
+# print('ten toi la', userName)
+# print("nam nay toi", age,'tuoi')
+# print('dia chi nha toi o', adress)
+
+# print(f'ten toi la {userName}, {age}, {adress}')
---
+++ b1_turtle.py
@@ -0,0 +1,22 @@
+from turtle import *
+
+pensize(5)
+forward(90)
+left(120)
+forward(90)
+left(120)
+forward(90)
+
+penup()
+color('blue')
+setposition(-10, -10)
+pendown()
+forward(90)
+left(120)
+forward(90)
+left(120)
+forward(90)
+# pos()
+
+print(pos())
+mainloop()
